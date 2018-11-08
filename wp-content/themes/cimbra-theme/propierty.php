26fde0a76ba33e2a03a23a2da03a9aa6f31df3ca
Gricel Dayhanna Sepúlveda Rozas <gr3c2l@gmail.com>
2018-11-08 17:44:49 -0300

    Catalogo progreso

--- wp-content/themes/cimbra-theme/propierty.php
+++ wp-content/themes/cimbra-theme/propierty.php
@@ -13,20 +13,28 @@
 
 <!-- FUNCTION QUERY DATA-->
 <?php
-  $postData = get_post();
+  $catalogId = get_cat_ID('catalogo');
+  $propiertyId = get_cat_ID('propiedad');
 
-  $obj = $postData[0];
-  echo $obj->ID;
-  echo $obj->post_author;
-
-  print_r($postData);
+  $args = array(
+    'posts_per_page'   => -1,
+    'category'         => $catalogId,
+    'orderby'          => 'date',
+    'order'            => 'ASC',
+  );
+  $posts = get_posts($args);
+  foreach( $posts as $post ) :  setup_postdata($post);
+  $m2 = get_post_meta($post->ID, 'm2', true);
 ?>
 
-
+  <li class="testimonial"><?php the_content(); ?></li><br/>
+  <li class="testimonial"><?php the_title(); ?></li><br/>
+  <span><?php echo $post->ID; ?></span>
+  <span><?php echo $m2 ?></span>
+ 
+<?php endforeach; ?>
 <!--CONTACT SECTION-->
-<?php
-  //include('contact.php');
-?>
+
 
 <!--FOOTER THEME-->
 <?php

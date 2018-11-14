75a647c335d6c08f20496ffe5c2ffa507818f540
Pablo Jacobi <>
2018-11-14 15:28:29 -0300

    Ready PHP contact form

---
+++ mailer/mailer.php
@@ -0,0 +1,41 @@
+<?php
+	require_once("Exception.php");
+	require_once("SMTP.php");
+	require_once("PHPMailer.php");
+	try {
+		$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$phone = $_POST['phone'];
+		$user_message = $_POST['message'];
+
+		//Luego tenemos que iniciar la validación por SMTP:
+		$mail->IsSMTP();
+		$mail->SMTPAuth = true;
+		//Si activas esto se mostrarán todos los logs de errores
+		//$mail->SMTPDebug = 2;
+		$from = "pablobichin@cimbrapropiedades.cl"; // Correo desde el cual se mandan las solicitudes
+		$mail->SMTPSecure = 'tls';
+		$mail->Host = "smtp.dreamhost.com"; // SMTP a utilizar. Por ej. smtp.elserver.com
+		$mail->Username = $from; // Correo completo a utilizar
+		$mail->Password = "bichinisimo30"; // Contraseña
+		$mail->Port = 587; // Puerto a utilizar
+		$mail->AddReplyTo($_POST['email'], $_POST['name']);
+		$mail->SetFrom($from, $_POST['name']);
+		$mail->AddAddress("nettox@gmail.com"); // Destinatario, debe ser el correo del cliente al cual recibe las solicitudes
+		$mail->IsHTML(false); // El correo se envía como HTML
+		$mail->Subject = $_POST['subject']; // Este es el titulo del email.
+		$body =  "Nombre: " . $name . "\n\n" .
+		  "Email Address: " . $email . "\n\n" .
+		  "Telefono: " . $phone . "\n\n" .
+		  "Mensaje: " . $user_message ;
+		$mail->Body = $body; // Mensaje a enviar
+		$exito = $mail->Send(); // Envía el correo.
+		header('Location: ' . str_replace('?r', "", $_SERVER['HTTP_REFERER'])."?r");
+	} catch (Exception $e) {
+	    echo $e->errorMessage(); //Pretty error messages from PHPMailer
+	} catch (\Exception $e) { //The leading slash means the Global PHP Exception class will be caught
+	    echo $e->getMessage(); //Boring error messages from anything else!
+	}
+?>
--- wp-content/themes/cimbra-theme/contact.php
+++ wp-content/themes/cimbra-theme/contact.php
@@ -1,16 +1,24 @@
+
 <!--CONTACT-->
 <section class="cm-contact" id="contacto">
   <div class="flex row between cm-flex-contact">
+    
+    <?php
+      if(isset($_GET['r'])){
+        echo '<div id="sentMessage">Mensaje enviado exitosamente</div>';
+      }
+    ?>
     <h2 class="flex row start">Escríbenos y conversemos acerca de tu espacio.</h2>
-    <form action="mailer.php" class="flex col end cm-contact-form" method="post">
+    <form action="/mailer/mailer.php" class="flex col end cm-contact-form" method="post">
       <h4>Formulario de Contacto</h4>
-      <div class="flex row end cm-flex-form"><label>Nombre:*</label><input id="name" name="name" type="text" /></div>
-      <div class="flex row end cm-flex-form"><label>Teléfono:*</label><input id="phone" name="phone" type="text" /></div>
-      <div class="flex row end cm-flex-form"><label>Correo:*</label><input id="email" name="email" type="email" /></div>
-      <div class="flex row end cm-flex-form"><label>Asunto:</label><input id="subject" name="subject" type="text" /></div>
-      <div class="flex row end cm-flex-form"><label>Mensaje:</label><textarea id="message" name="message" rows="7"></textarea></div><button
+      <div class="flex row end cm-flex-form"><label>Nombre:*</label><input id="name" name="name" type="text" required placeholder="Introduce tu nombre"/></div>
+      <div class="flex row end cm-flex-form"><label>Teléfono:*</label><input id="phone" name="phone" type="text" type="text" required placeholder="Introduce tu teléfono"/></div>
+      <div class="flex row end cm-flex-form"><label>Correo:*</label><input id="email" name="email" type="email" type="text" required placeholder="Introduce una dirección de correo válida"/></div>
+      <div class="flex row end cm-flex-form"><label>Asunto:</label><input id="subject" name="subject" type="text" type="text" required placeholder="Ingresa el asunto"/></div>
+      <div class="flex row end cm-flex-form"><label>Mensaje:</label><textarea id="message" name="message" rows="7" type="text" required placeholder="Ingresa el mensaje"></textarea></div><button
         class="normal" type="submit">Enviar</button><span class="cm-note">Te contactaremos de vuelta en un plazo max.
         de 24hrs (hábiles)</span>
     </form>
   </div>
 </section>
+

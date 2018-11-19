5c6ed31c81d28548642443e9397368b3f7f1cb2d
Gricel Dayhanna Sepúlveda Rozas <gr3c2l@gmail.com>
2018-11-19 17:30:06 -0300

    Revert "bd config 2"
    This reverts commit dfc3b47c175bfd6a72c5ac68be042bf06b969e02.

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'cimbrapropieadescm');
+define('DB_NAME', 'cimbrapropieades-cm');
 
 /** MySQL database username */
 define('DB_USER', 'jimenafuentealba');
@@ -29,7 +29,7 @@ define('DB_USER', 'jimenafuentealba');
 define('DB_PASSWORD', 'jimena2018');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql.cimbrapropiedades.cl');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');

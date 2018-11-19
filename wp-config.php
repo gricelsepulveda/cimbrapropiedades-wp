5f5357a0fcab27062380c07058e9b0ea942cec4d
Gricel Dayhanna Sepúlveda Rozas <gr3c2l@gmail.com>
2018-11-19 17:44:16 -0300

    url fix

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,9 @@
  * @package WordPress
  */
 
+define( 'WP_HOME', 'http://www.cimbrapropiedades.cl' );
+define( 'WP_SITEURL', 'http://www.cimbrapropiedades.cl' );
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'cimbrapropieadescm');

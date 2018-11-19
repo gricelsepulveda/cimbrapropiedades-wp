7d1b38de0b2ea77292baa80cd0dfa4e572f12cc9
Gricel <gr3c2l@gmail.com>
2018-11-19 12:15:12 -0300

    cambios en la configuración del correo

--- mailer/mailer.php
+++ mailer/mailer.php
@@ -15,15 +15,15 @@
 		$mail->SMTPAuth = true;
 		//Si activas esto se mostrarán todos los logs de errores
 		//$mail->SMTPDebug = 2;
-		$from = "pablobichin@cimbrapropiedades.cl"; // Correo desde el cual se mandan las solicitudes
+		$from = "contacto@cimbrapropiedades.cl"; // Correo desde el cual se mandan las solicitudes
 		$mail->SMTPSecure = 'tls';
 		$mail->Host = "smtp.dreamhost.com"; // SMTP a utilizar. Por ej. smtp.elserver.com
 		$mail->Username = $from; // Correo completo a utilizar
-		$mail->Password = "bichinisimo30"; // Contraseña
+		$mail->Password = "jimena2018"; // Contraseña
 		$mail->Port = 587; // Puerto a utilizar
 		$mail->AddReplyTo($_POST['email'], $_POST['name']);
 		$mail->SetFrom($from, $_POST['name']);
-		$mail->AddAddress("nettox@gmail.com"); // Destinatario, debe ser el correo del cliente al cual recibe las solicitudes
+		$mail->AddAddress("jimena.fuentealba@cimbrapropiedades.cl"); // Destinatario, debe ser el correo del cliente al cual recibe las solicitudes
 		$mail->IsHTML(false); // El correo se envía como HTML
 		$mail->Subject = $_POST['subject']; // Este es el titulo del email.
 		$body =  "Nombre: " . $name . "\n\n" .

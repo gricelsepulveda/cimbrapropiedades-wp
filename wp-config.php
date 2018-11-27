0cc6a1981ca8f6887539e609b1970eea778a797c
Gricel <gr3c2l@gmail.com>
2018-11-27 07:20:59 -0300

    bd settings dreamhost

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'cimbrapropieadescm');
+define('DB_NAME', 'cimbracm');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'jimenafuentealba');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'jimenafuentealba2018');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.cimbrapropiedades.cl');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
